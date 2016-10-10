b10bf99c60fcb9be72e3705fbbf07b9e8ce3fa1f
ahawrylak <adrian.hawrylak@gmail.com>
2016-10-10 18:37:59 +0200

    Generate Review model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161008145005) do
+ActiveRecord::Schema.define(version: 20161010163246) do
 
   create_table "books", force: :cascade do |t|
     t.string   "author"
@@ -19,6 +19,16 @@ ActiveRecord::Schema.define(version: 20161008145005) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
---
+++ test/models/review_test.rb
@@ -0,0 +1,6 @@
+require 'test_helper'
+
+class ReviewTest < ActiveSupport::TestCase
+
+
+end
