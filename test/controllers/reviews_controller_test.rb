5b25bef4a63f5d76be6b9fb258a8e5e38cb1ec9c
ahawrylak <adrian.hawrylak@gmail.com>
2016-10-10 22:17:35 +0200

    Reviews controller tests

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -1,19 +1,42 @@
 require 'test_helper'
 
 class ReviewsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
+
+
+  test 'should redirect new when not signed in' do
     get reviews_new_url
-    assert_response :success
+    assert_redirected_to new_user_session_url
   end
 
-  test "should get create" do
-    get reviews_create_url
-    assert_response :success
+  test 'should redirect create when not signed in' do
+    review_params = { review: { content: "I liked this book a lot.",
+                                user: users(:user1),
+                                book: books(:book1) } }
+    assert_no_difference 'Review.count' do
+      post reviews_url, params: review_params
+    end
+    assert_not flash[:alert].empty?
+    assert_redirected_to new_user_session_url
   end
 
-  test "should get destroy" do
-    get reviews_destroy_url
-    assert_response :success
+  test 'should redirect destroy when not signed in' do
+    assert_no_difference 'Review.count' do
+      delete review_url(reviews(:review1))
+    end
+    assert_not flash[:alert].empty?
+    assert_redirected_to new_user_session_url
   end
+
+  test 'should not allow user deleting others review' do
+    review = reviews(:review1)
+    other_user = users(:user2)
+    assert_not_equal other_user, review.user
+    sign_in other_user
+    assert_no_difference "Review.count" do
+      delete review_url(review)
+    end
+    assert_not flash[:alert].empty?
+  end
+
 
 end
