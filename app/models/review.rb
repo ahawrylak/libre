2f15a62d9d03036ee88e76fce7514a2fe6ee4c0a
ahawrylak <adrian.hawrylak@gmail.com>
2016-10-10 19:15:59 +0200

    Add Review model validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,10 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :content, presence: true
+  validates :user_id, presence: true
+  validates :book_id, presence: true
+  validates_associated :user, :book
+
 end
