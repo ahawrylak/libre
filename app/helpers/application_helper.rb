82fda3f16f4a9ed24dffd09a28cad55736c14b27
ahawrylak <adrian.hawrylak@gmail.com>
2016-10-11 15:25:24 +0200

    Styling fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,17 @@
 module ApplicationHelper
 
+  # Converts DateTime object to human readable format
   def convert_time(datetime)
     datetime.to_formatted_s(:long)
+  end
+
+  # Shortens given text if it's too long to display
+  def shorten(text)
+    if text.length > 50
+      text[0...50] + '...'
+    else
+      text
+    end
   end
 
 end
