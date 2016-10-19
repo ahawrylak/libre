9d51eedf15f080ece1c7936039cdecd2d2e3ab11
ahawrylak <adrian.hawrylak@gmail.com>
2016-10-19 17:19:23 +0200

    Rating model validations

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,12 @@
 class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :user_id, presence: true
+  validates :book_id, presence: true
+  validates :value, presence: true, numericality: { only_integer: true,
+                                                    greater_than_or_equal_to: 1,
+                                                    less_than_or_equal_to: 5 }
+  validates_associated :user, :book
+
 end
