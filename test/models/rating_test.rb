f5ea2db4f18414e5e3c5ae1688fd62b5bb95e8aa
ahawrylak <adrian.hawrylak@gmail.com>
2016-10-19 17:14:35 +0200

    Add rating model tests

--- test/models/rating_test.rb
+++ test/models/rating_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class RatingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @rating = ratings(:rating1)
+    assert @rating.valid?
+  end
+
+  test 'rating should be assocciated with user' do
+    @rating.user = nil
+    assert_not @rating.valid?
+  end
+
+  test 'rating should be assocciated with book' do
+    @rating.book = nil
+    assert_not @rating.valid?
+  end
+
+  test 'value should be present' do
+    @rating.value = nil
+    assert_not @rating.valid?
+  end
+
+  test 'value should be between 1 and 5' do
+    @rating.value = 0
+    assert_not @rating.valid?
+    @rating.value = 6
+    assert_not @rating.valid?
+  end
+
+
 end
